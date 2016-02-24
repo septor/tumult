06742fc06d042c632d1172171124f5c07a1d638a
septor <patrickweaver@gmail.com>
2016-02-24 16:49:55 -0500

    config tweaks, index expansion

--- index.php
+++ index.php
@@ -3,8 +3,24 @@
  * Tumult; get more information at:  http://tumultget.xyz/
  * For contributions, copyrights, and more view the `docs` folder.
  */
+if(!file_exists('config/master.php')
+{
+	die('You need to configure and rename master.example.php to master.php for Tumult to function.');
+}
 include('_libs.php');
-
 $cp = new Statics();
+$te = new Theme(TUMULT_THEME);
+
+echo '<html>
+	<head>
+		<title>'.TUMULT_NAME.'title>
+	</head>
+	<body>';
+
+$te->parseSc('themes/'.TUMULT_THEME.'/template.html');
+
+echo '
+	</body>
+</html>';
 
 echo $cp->content('statics/about.md');
