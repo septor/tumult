fb4c4f7207188d69e4447a25688d34a7bf1b2f13
Patrick Weaver <patrickweaver@gmail.com>
2022-04-02 15:42:06 -0400

    version file for future proofing

---
+++ _version.php
@@ -0,0 +1,7 @@
+<?php
+/*
+ * Tumult; get more information at:  https://github.com/septor/tumult
+ * For contributions, copyrights, and more view the `docs` folder.
+ */
+
+define('TUMULT_VERSION', 'alpha3')
