df196cde6d1aea4c3e981c6604ffc29839d24db0
Patrick Weaver <patrickweaver@gmail.com>
2022-04-16 12:11:36 -0400

    better make sure they're defined so we don't throw errors

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
  * Tumult; get more information at:  https://github.com/septor/tumult
  * For contributions, copyrights, and more view the `docs` folder.
  */
+
 if(!file_exists('config/master.php'))
 	die('You need to configure and rename master.example.php to master.php for Tumult to function.');
 
@@ -20,9 +21,9 @@ echo '<!DOCTYPE html>
 <html lang="en">
 	<head>
 		<meta charset="UTF=8">
-		<meta name="description" content="'.TUMULT_META_DESCRIPTION.'">
-        <meta name="keywords" content="'.TUMULT_META_KEYWORDS.'">
-        <meta name="author" content="'.TUMULT_META_AUTHOR.'">
+		'.(defined(TUMULT_META_DESCRIPTION ? '<meta name="description" content="'.TUMULT_META_DESCRIPTION.'">' : '').'
+        '.(defined(TUMULT_META_KEYWORDS ? '<meta name="keywords" content="'.TUMULT_META_KEYWORDS.'">' : '').'
+        '.(defined(TUMULT_META_AUTHOR ? '<meta name="author" content="'.TUMULT_META_AUTHOR.'">' : '').'
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
 		<title>'.TUMULT_SITENAME.'</title>
